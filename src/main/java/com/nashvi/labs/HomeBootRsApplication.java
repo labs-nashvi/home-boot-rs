8010e10f375d1a9d725aec170c478ab1c1b9958b
Nashvi Labs <labs.nashvi@gmail.com>
2017-01-15 01:25:29 -0500

    Add rest home page access for root path

--- src/main/java/com/nashvi/labs/HomeBootRsApplication.java
+++ src/main/java/com/nashvi/labs/HomeBootRsApplication.java
@@ -1,11 +1,23 @@
 package com.nashvi.labs;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@EnableAutoConfiguration
 @SpringBootApplication
 public class HomeBootRsApplication {
 
+	@RequestMapping("/")
+	@ResponseBody
+	public String home(){
+		return "You have reached the homepage of home-boot";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HomeBootRsApplication.class, args);
 	}
