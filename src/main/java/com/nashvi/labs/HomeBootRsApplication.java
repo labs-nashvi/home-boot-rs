e5ea400ed90e110425c16c5213ff8358d7d487f9
Nashvi Labs <labs.nashvi@gmail.com>
2017-01-15 00:31:54 -0500

    First commit

---
+++ src/main/java/com/nashvi/labs/HomeBootRsApplication.java
@@ -0,0 +1,12 @@
+package com.nashvi.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBootRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBootRsApplication.class, args);
+	}
+}
